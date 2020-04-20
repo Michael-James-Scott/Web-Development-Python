fc6845700601a70ccfb6885dac0a4305cb90a61d
Mike Scott <mikescott9909@gmail.com>
2020-04-20 15:21:57 -0500

    added entry model

---
+++ MyShell.py
@@ -0,0 +1,27 @@
+import os 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_log.settings")
+
+import django
+django.setup()
+
+from learning_logs.models import Topic
+topics = Topic.objects.all()
+
+for topic in topics:
+    print(topic.id,topic)
+
+# if you know the id of a particular object, you can use the topics.models.get()
+# method to retrieve the object and examine its attributes
+
+t = Topic.objects.get(id=2)
+print(t.text)
+print(t.date_added)
+
+
+#to get data through a foreign key relationship, you use the lowercase name of the 
+# related model followed by an underscore and the word set.
+
+entries = t.entry_set.all()
+
+for entry in entries:
+    print(entry)
