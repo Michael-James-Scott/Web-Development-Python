d4764a875870d1787f2692dc9a20989fa20a7713
Mike Scott <mikescott9909@gmail.com>
2020-04-20 15:51:33 -0500

    added html base, index, and topic pages

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """ The home page for Learning Log"""
+
+    return render(request,'learning_logs/index.html')
+
+def topics(request):
+    """ The home page for Learning Log"""
+
+    return render(request,'learning_logs/topics.html')
+
